82be620df3f5be6749f211bb1e7c651194e22393
brivu <emailbrian.vu@gmail.com>
2020-11-17 00:01:15 -0800

    Connect server to database.  Get request to '/items/images' endpoing is working

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const db = require('../db/index.js');
+const pool = require('./../db/index.js');
 const port = 3006;
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/item/images', (req, res)=>{
+  pool.query('SELECT * FROM image_urls', (err, data) => {
+    if (err){
+      res.status(400).send(err);
+    } else {
+      res.status(200).send(data)
+    }
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
