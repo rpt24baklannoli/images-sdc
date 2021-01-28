79ef93e17746b983727ecaa76b6dd9fa8bc5c045
Gabriel Gutierrez <gabe.grez@gmail.com>
2021-01-28 11:09:08 -0800

    Remove comments and add varchar limit

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,7 @@
--- DROP DATABASE IF EXISTS images;
--- CREATE DATABASE images;
-
 DROP DATABASE IF EXISTS fetsy;
 CREATE DATABASE fetsy;
---\fetsy
 \c fetsy;
 
--- CREATE TABLE IF NOT EXISTS image_urls (
 CREATE TABLE IF NOT EXISTS images (
   id SERIAL PRIMARY KEY,
   item_id INTEGER,
