b2f0ea05570df31bf1d75f7537d6c282a709fef1
Gabriel Gutierrez <gabe.grez@gmail.com>
2021-01-30 01:19:38 -0800

    Refactored model to query using values array for pg

--- model/index.js
+++ model/index.js
@@ -3,7 +3,8 @@ const pool = require('../db/index.js');
 module.exports = {
   images: {
     getOne: (item_id) => {
-      return pool.query(`SELECT * FROM images WHERE item_id = ${item_id};`);
+      let values = [item_id];
+      return pool.query(`SELECT * FROM images WHERE item_id = ($1);`, values);
     },
     getAll: () => {
       return pool.query(`SELECT * FROM images;`);
@@ -12,13 +13,16 @@ module.exports = {
       return pool.query('SELECT DISTINCT ON (item_id) item_id, image_url FROM images');
     },
     post: (item_id, url) => {
-      return pool.query(`INSERT INTO images (item_id, image_url) VALUES (${item_id}, ${url});`);
+      let values = [item_id, url];
+      return pool.query(`INSERT INTO images (item_id, image_url) VALUES ($1,$2) RETURNING *;`, values);
     },
     update: (record_id, url) => {
-      return pool.query(`UPDATE images SET image_url = ${url} WHERE id = ${record_id} RETURNING *;`);
+      let values = [url, record_id];
+      return pool.query(`UPDATE images SET image_url = ($1) WHERE id = ($2) RETURNING *;`, values);
     },
     delete: (record_id) => {
-      return pool.query(`DELETE FROM images WHERE id = ${record_id};`);
+      let values = [record_id];
+      return pool.query(`DELETE FROM images WHERE id = ($1) RETURNING *;`, values);
     }
   }
 }
