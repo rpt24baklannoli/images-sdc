3849abcf539a11a763b69316456b3c91a8129617
Gabriel Gutierrez <gabe.grez@gmail.com>
2021-02-02 20:16:14 -0800

    Attempt 16

--- db/index.js
+++ db/index.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool;
 require('dotenv').config({ path: '../config/.env' });
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
+  user: process.env.POSTGRES_USER || 'gabriel.g',
   host: 'localhost',
   database: 'fetsy',
-  password: process.env.POSTGRES_PASSWORD,
+  password: process.env.POSTGRES_PASSWORD || '',
   port: 5432
 });
 
