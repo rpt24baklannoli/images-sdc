0f039f687447d042280667cb1c6a53c7af38e6c8
Gabriel Gutierrez <gabe.grez@gmail.com>
2021-02-02 23:04:50 -0800

    Add catch for no aws credentials

--- db/seeding/dataGen.js
+++ db/seeding/dataGen.js
@@ -8,7 +8,22 @@ const randomNumber = (min, max) => {
 const writeCSV =  async (numberOfItemsToSeed) => {
   let writer = fs.createWriteStream('./db/seeding/seedData.csv');
   writer.write(`item_id,image_url\n`)
-  let urls = await S3.getUrlsS3();
+
+  let urls;
+
+  try {
+    //for user with credentials
+    urls = await S3.getUrlsS3();
+  } catch {
+    //for dummy urls
+    urls = [
+    "https://fetsy-images-sdc.s3-us-west-1.amazonaws.com/photo-1527613426441-4da17471b66d.jpeg",
+    "https://fetsy-images-sdc.s3-us-west-1.amazonaws.com/photo-1595824228556-440f8776f97d.jpeg",
+    "https://fetsy-images-sdc.s3-us-west-1.amazonaws.com/photo-1576497065840-32541d4780c5.jpeg",
+    "https://fetsy-images-sdc.s3-us-west-1.amazonaws.com/photo-1549187774-b4e9b0445b41.jpeg",
+    "https://fetsy-images-sdc.s3-us-west-1.amazonaws.com/photo-1525459819821-1c2d33189e23.jpeg"
+    ];
+  }
 
   for (let i = 0; i < numberOfItemsToSeed; i++){
     let itemId = i + 1;
