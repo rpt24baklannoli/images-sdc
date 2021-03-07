27e97fe34b9c51f9d61baf6d8d3600891b83490d
Gabriel Gutierrez <gabe.grez@gmail.com>
2021-03-06 21:34:19 -0800

    Reorg files

--- db/index.js
+++ db/index.js
@@ -1,11 +1,19 @@
 const Pool = require('pg').Pool;
 require('dotenv').config({ path: '../config/.env' });
 
+// const pool = new Pool({
+//   user: process.env.POSTGRES_USER || 'gabriel.g',
+//   host: 'localhost',
+//   database: 'fetsy',
+//   password: process.env.POSTGRES_PASSWORD || '',
+//   port: 5432
+// });
+
 const pool = new Pool({
-  user: process.env.POSTGRES_USER || 'gabriel.g',
-  host: 'localhost',
+  user: 'postgres',
+  host: '13.57.236.157',
   database: 'fetsy',
-  password: process.env.POSTGRES_PASSWORD || '',
+  password: 'sdcfetsy',
   port: 5432
 });
 
