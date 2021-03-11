f684a10ad7b75664ab16dbf29fad4707df00292f
Gabriel Gutierrez <gabe.grez@gmail.com>
2021-03-10 21:33:12 -0800

    Add redis caching

--- server/index.js
+++ server/index.js
@@ -6,6 +6,14 @@ const pool = require('./../db/index.js');
 const controller = require('../controller/index.js');
 const app = express();
 
+const redis = require('redis');
+const client = redis.createClient();
+// client.on('error', (err) => {
+//   console.log("Error " + err);
+// });
+client.on('connect', (err) => {
+  console.log("Connected to redis");
+});
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,13 +22,23 @@ app.use('/items/:item_id', express.static(__dirname + '/../react-client/dist'));
 
 app.get('/items/:item_id/images', (req, res) => {
   const id = req.params.item_id;
-  controller.images.getOne(id)
-    .then((response) => {
-      res.send(response);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+  return client.get(`images:${id}`, (err, result) => {
+    if (err) {
+      return res.status(500).send(error);
+    }
+    if (result) {
+      return res.send(JSON.parse(result));
+    } else {
+      controller.images.getOne(id)
+        .then((response) => {
+          client.set(`images:${id}`, JSON.stringify(response));
+          return res.send(response);
+        })
+        .catch((error) => {
+          return res.status(400).send(error);
+        });
+    }
+  })
 });
 
 app.post('/items/:item_id/images', (req, res) => {
